4dc27add8f49fab55c198d9e94a152f0c643e2a6
David Oesch <david.oesch@gmail.com>
2025-11-24 16:48:47 +0100

    aaded local_assets to store the GRI and added it to gitignore. as well in dev_config

--- main_functions/main_mosaicing.py
+++ main_functions/main_mosaicing.py
@@ -577,14 +577,14 @@ def equalize_all_extents(
     else:
         raise FileNotFoundError(f"No cloud probability file found matching: {csplus_pattern}")
 
-    # Add omnicloud file
-    omnicloud_pattern = os.path.join(data_dir, f"{config.AROSICS_CONFIG['singleband_mosaic_pattern']}_omnicloud.tif")
-    omnicloud_matches = glob.glob(omnicloud_pattern)
+    # # Add omnicloud file
+    # omnicloud_pattern = os.path.join(data_dir, f"{config.AROSICS_CONFIG['singleband_mosaic_pattern']}_omnicloud.tif")
+    # omnicloud_matches = glob.glob(omnicloud_pattern)
 
-    if omnicloud_matches:
-        files_to_process.extend(omnicloud_matches)
-    else:
-        raise FileNotFoundError(f"No omnicloud file found matching: {omnicloud_pattern}")
+    # if omnicloud_matches:
+    #     files_to_process.extend(omnicloud_matches)
+    # else:
+    #     raise FileNotFoundError(f"No omnicloud file found matching: {omnicloud_pattern}")
 
     # Add reference image if it exists
     reference_image = config.AROSICS_CONFIG['reference_image']
