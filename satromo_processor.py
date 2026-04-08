88f0b0abd7f03a14e10902fa39e830d0384c4d56
Joan Sturm <joan.sturm@gmail.com>
2026-04-08 11:16:24 +0200

    Update satromo_processor.py
    - Added script for vhi processing
    - changed regions of interest to EPSG:2056 coordinates
    - default roi should be None for operational use (uppermost example)
    - added testing
    1) if input S2-SR exists
    2) if VHI already exists (can be reprocessed regardless with the forcing)

--- satromo_processor.py
+++ satromo_processor.py
@@ -3,7 +3,7 @@ import datetime
 
 import configuration as config
 from step0_functions import get_step0_dict, step0_main
-from step1_processors import step1_processor_s2_sr#, step1_processor_vhi #, step1_processor_l57_toa, step1_processor_l89_sr, step1_processor_l89_toa, step1_processor_s3_toa, step1_processor_vhi_hist
+from step1_processors import step1_processor_s2_sr, step1_processor_vhi #, step1_processor_l57_toa, step1_processor_l89_sr, step1_processor_l89_toa, step1_processor_s3_toa, step1_processor_vhi_hist
 from main_functions import main_utils
 
 
@@ -110,30 +110,47 @@ if __name__ == "__main__":
                     result = f"PRODUCT_S2_LEVEL_2A: STAC items already exist for date {current_date_str}, skipping processing."
 
             elif product_to_be_processed == 'PRODUCT_VHI':
-                roi = ee.Geometry.Rectangle(config.ROI_RECTANGLE) # TODO: doesn't work with GEE anymore, must be fixed
-                # roi = ee.Geometry.Rectangle(
-                #     [8.10, 47.18, 8.20, 47.25])  # 6221 Rickenbach
-                # roi = ee.Geometry.Rectangle(
-                #    [7.81, 46.35, 8.06, 46.46])  # Oberaletschgletscher
-                # roi = ee.Geometry.Rectangle(
-                #     [7.16, 47.20, 7.27, 47.24])  # Tavannes
-                # roi = ee.Geometry.Rectangle(
-                #     [8.06, 47.14, 8.72, 47.18])  # Raten ZG/SZ
-                result = step1_processor_vhi.process_PRODUCT_VHI(
-                    roi, collection_ready, current_date_str)
+                roi = None # Default for operational mode
+                # For testing, we can set a specific ROI --> roi = (b.left, b.bottom, b.right, b.top) 
+                # roi = (2681000, 1230100, 2687500, 1237900) # Sihlwald
+                roi = (2743000, 1224000, 2748900, 1229500) # Wildhaus (Orbitgrenze)
+                # roi = (2573000, 1199600, 2583100, 1208400) # Kerzersmoos
+                # roi = (2534500, 1194100, 2550700, 1203300) # Val de Travers
+
+                # Check 1: Does the INPUT (S2-SR) exist? If not, VHI cannot be calculated.
+                api_path = getattr(config, 'STAC_FSDI_API')
+                collection = getattr(config, 'PRODUCT_VHI')['step0_collection']
+                stac_catalog_url, collection_id = main_utils.extract_collection_id_from_url(collection, api_path)
+                daily_items = main_utils.get_stac_items_for_date(
+                    stac_catalog_url, collection_id,
+                    datetime.datetime.strptime(current_date_str, "%Y-%m-%d").date()
+                )
+                if len(daily_items) == 0:
+                    print(f"VHI: No S2-SR input data for {current_date_str}, skipping.")
+                    result = f"VHI: No S2-SR input data for {current_date_str}, skipping."
+                else:
+                    # Check 2: Does the OUTPUT (VHI) already exist? If yes, we can skip processing (unless force_reprocess is enabled)
+                    vhi_collection = getattr(config, 'PRODUCT_VHI')['step1_collection'] 
+                    stac_catalog_url_vhi, collection_id_vhi = main_utils.extract_collection_id_from_url(vhi_collection, api_path)
+                    daily_items_vhi = main_utils.get_stac_items_for_date(
+                        stac_catalog_url_vhi, collection_id_vhi,
+                        datetime.datetime.strptime(current_date_str, "%Y-%m-%d").date()
+                    )
+                    if len(daily_items_vhi) == 0:
+                        print(f"VHI: No existing VHI STAC items for {current_date_str}, processing VHI.")
+                        result = step1_processor_vhi.process_product_vhi(
+                        current_date_str, collection_ready, roi)
+                    elif force_reprocess:
+                        print(f"Force reprocess enabled: reprocessing {current_date_str} despite existing STAC items.")
+                        result = step1_processor_vhi.process_product_vhi(
+                            current_date_str, collection_ready, roi)
+                    else:
+                        print(f"STAC items already exist for date {current_date_str}: skipping processing.")
+                        result = f"VHI: STAC items already exist for date {current_date_str}, skipping processing."
 
             # elif product_to_be_processed == 'PRODUCT_VHI_HIST':
-            #     roi = ee.Geometry.Rectangle(config.ROI_RECTANGLE)
-            #     # roi = ee.Geometry.Rectangle(
-            #     #     [8.06, 47.14, 8.72, 47.18])  # Raten ZG/SZ
-            #     # roi = ee.Geometry.Rectangle(
-            #     #     [9.41, 46.83, 9.65, 47.02])  # Chur/Landquart
-            #     # roi = ee.Geometry.Rectangle(
-            #     #     [6.40, 46.47, 6.81, 46.61])  # Lausanne VD
             #     result = step1_processor_vhi_hist.process_PRODUCT_VHI_HIST(
             #         roi, current_date_str)
-
-
 
             elif product_to_be_processed == 'PRODUCT_MSG_CLIMA':
                 # roi = ee.Geometry.Rectangle(
