39a5c053dc8840ff0d1d14a7affd659490e8a51a
SARcycle <57989133+SARcycle@users.noreply.github.com>
2026-04-29 14:42:11 +0200

    Update DSM file path and enhance terrain mask processing in step1_processor_s2_sr.py

--- configuration/dev_config.py
+++ configuration/dev_config.py
@@ -34,7 +34,7 @@ BUFFER = os.path.join("assets", "swissboundary_buffer_5000m.gpkg")
 OVERVIEW_LAKES = os.path.join("assets", "overview_lakes_2056.gpkg")
 OVERVIEW_RIVERS = os.path.join("assets", "overview_rivers_2056.gpkg")
 
-DSM_FILE=os.path.join("local_assets","DSM_full_CH_nodata.tif")
+DSM_FILE=os.path.join("local_assets","DSM_10m_EPSG2056_CH_clipped_10km_extended_9999.tif")
 
 
 ## PRODUCTS, INDICES and custom COLLECTIONS ###
